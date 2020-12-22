1af94021ddf4abb398616967b76a316c32edb8de
Jonah <jonahcheyette@gmail.com>
2020-12-21 23:33:03 -0800

    Setting up the basics, will do more tommorrow

---
+++ Assets/Scripts/RayMarchingMaster.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RayMarchingMaster : MonoBehaviour {
+    public ComputeShader RayMarchingShader;
+    private RenderTexture _target;
+
+    //called every frame by unity, which automatically passes in what's already rendered as the source and the camera's target (in most cases, the screen) as the destination
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination) {
+        // Make sure we have a current render target
+        InitRenderTexture();
+
+        // Set the target and dispatch the compute shader
+        RayMarchingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 4f);
+        RayMarchingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+
+    private void InitRenderTexture() {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height) {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
