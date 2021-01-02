5925f566aea86f12ac145c69f9e40c87f38906e6
Jonah <jonahcheyette@gmail.com>
2021-01-01 17:00:46 -0800

    added a way to color the outline

--- Assets/Scripts/OutlineRayMarchingMaster.cs
+++ Assets/Scripts/OutlineRayMarchingMaster.cs
@@ -9,9 +9,14 @@ public class OutlineRayMarchingMaster : BaseRayMarchingMaster {
     [Min(0)]
     public float outlineSize;
 
+    public Color inner;
+    public Color outer;
+
     public override void SetDynamicShaderParameters() {
         base.SetDynamicShaderParameters();
         rayMarchingShader.SetFloat("outlineSize", outlineSize);
         rayMarchingShader.SetFloat("outlineStrength", outlineStrength);
+        rayMarchingShader.SetFloats("inner", new float[] { inner.r, inner.g, inner.b });
+        rayMarchingShader.SetFloats("outer", new float[] { outer.r, outer.g, outer.b });
     }
 }
